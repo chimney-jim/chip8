d28a0b04a95dd11e13be88ad32b2a327be77108c
Jim Fitzgerald <jim@schoolzilla.com>
2014-12-23 23:27:08 -0500

    removed cpu_atom since i now know how to use reset! and swap! properly.  added a shitty mechanism for set and dec of both timers

--- src/chip8/cpu.clj
+++ src/chip8/cpu.clj
@@ -4,41 +4,66 @@
 (defrecord CPU [opcode memory Vreg Ireg pc gfx delay-timer sound-timer stack sp key])
 
 (defn build-cpu []
-  (map->CPU {:opcode nil,
+  "build a prepped cpu"
+  (map->CPU {:opcode 0,
              :memory (sized-vec 4096),
              :Vreg (sized-vec 16),
-             :Ireg nil,
-             :pc nil,
+             :Ireg 0,
+             :pc 0x200,
              :gfx (sized-vec (* 64 32)),
-             :delay-timer nil,
-             :sound-timer nil,
+             :delay-timer 0,
+             :sound-timer 0,
              :stack (sized-vec 16),
-             :sp nil,
+             :sp 0,
              :key (sized-vec 16)}))
 
 (defn change-opcode [cpu opcode]
   "put new opcode in cpu for dynamic var"
   (assoc-in cpu [:opcode] opcode))
 
+(defn dec-delay-sound-timers [cpu]
+  "decrement the delay and sound timers if their value is > 0"
+  (let [d-timer (:delay-timer cpu)
+        s-timer (:sound-timer cpu)]
+    (if ((or (= d-timer 0) (= s-timer 0)))
+      cpu
+      (-> cpu
+          (assoc-in [:delay-timer] dec)
+          (assoc-in [:sound-timer] dec))))))
+
+(defn set-delay-timer [cpu val]
+  (-> cpu (assoc-in [:delay-timer] val)))
+
+(defn set-sound-timer [cpu val]
+  (-> cpu (assoc-in [:sound-timer] val)))
+
 (defn mem-insert [cpu pos val]
   "insert value into memory"
-  (assoc-in cpu [:memory] (assoc (:memory cpu) pos val)))
+  (let [memory (:memory cpu)]
+    (assoc-in cpu [:memory] (assoc memory pos val))))
 
 (defn mem-get [cpu pos]
   "grabs the value at the position in memory"
   (-> cpu :memory (get pos)))
 
-(defn mem-get-next-opcode [cpu]
+(defn get-next-opcode [cpu]
+  "grab next opcode from memory at pc"
   (let [pc (:pc cpu)
         memory (:memory cpu)]
-    (bit-or (get memory pc) (get memory (+ pc 1)))))
+    (bit-or
+     (bit-shift-left (get memory pc) 8) (get memory (+ pc 1)))))
+
+(defn inc-pc [cpu]
+  "increments the pc by two since two spots in memory are needed to form an opcode"
+  (let [curr-pc (:pc cpu)]
+    (assoc-in cpu [:pc] (+ curr-pc 2))))
 
 (defn load-game [cpu game]
   "loads a game into the system")
 
 (defn emulate-cycle [cpu]
   "emulates one cycle of the cpu"
-  ;;fetch opcode
+  (get-next-opcode cpu)
   ;;decode opcode
   ;;execute opcode
 
@@ -47,7 +72,3 @@
 
 (defn set-keys [cpu]
   "stores a keypress for controls")
-
-(defn fetch-opcode [cpu]
-  "fetches an opcode from memory of the cpu"
-  ())
